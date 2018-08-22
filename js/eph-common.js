f097b37afe3eb0f8f515673ac7947456281abe35
Eugene Alvin Villar <seav80@gmail.com>
2018-08-22 17:34:08 +0800

    Support retina CARTO tiles

--- js/eph-common.js
+++ js/eph-common.js
@@ -16,7 +16,7 @@ const ADMIN_QIDS              = [COUNTRY_QID, REGION_QID, PROVINCE_QID, HUC_QID,
 const ADMIN_LEVELS            = 4;
 const OSM_LAYER_URL           = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const OSM_LAYER_ATTRIBUTION   = 'Base map &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
-const CARTO_LAYER_URL         = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_labels_under/{z}/{x}/{y}.png';
+const CARTO_LAYER_URL         = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png';
 const CARTO_LAYER_ATTRIBUTION = 'Base map &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> (data), <a href="https://carto.com/">CARTO</a> (style)';
 const TILE_LAYER_MAX_ZOOM     = 19;
 const MIN_PH_LAT              =   4.5;
