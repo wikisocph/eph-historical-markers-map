715b6cf8163b1f9f15e56d1de97795c9455020cb
Eugene Alvin Villar <seav80@gmail.com>
2024-02-13 12:23:44 +0800

    Ignore retired markers

--- js/eph-historical-markers-globals.js
+++ js/eph-historical-markers-globals.js
@@ -19,6 +19,7 @@ const SPARQL_QUERY_0 =
           p:P625 ?coordStatement .
   ?coordStatement ps:P625 ?coord .
   FILTER NOT EXISTS { ?coordStatement pq:P582 ?endTime }
+  FILTER NOT EXISTS { ?marker wdt:P576 [] }
   FILTER (!ISIRI(?coord)) .
   BIND (SUBSTR(STR(?marker), 32) AS ?markerQid)
 }`;
